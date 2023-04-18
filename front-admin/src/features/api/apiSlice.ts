19ce5695ca7c4d540c50f2aac9d02ced5d9c390f
Jeanluca <jeanlucafp@gmail.com>
2023-04-18 16:16:11 -0300

    KC k8s and CI front

--- front-admin/src/features/api/apiSlice.ts
+++ front-admin/src/features/api/apiSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const baseUrl = "http://localhost:8080/api";
+export const baseUrl = "http://137.184.245.6:8080/api";
 
 export const apiSlice = createApi({
   reducerPath: "api",
